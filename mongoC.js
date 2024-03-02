5581fbf5fb25e7ecfbc26c71e9d07b01ba74e1fe
Mrunal00712 <117890321+Mrunal00712@users.noreply.github.com>
2024-03-02 22:56:35 +0530

    Update mongoC.js

--- mongoC.js
+++ mongoC.js
@@ -1,14 +1,14 @@
 import { MongoClient } from "mongodb";
 
 const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
-const connectionString = `mongodb+srv://mrunalugemuge:<password>@devcluster.iriyz7t.mongodb.net/?retryWrites=true&w=majority&appName=DevCluster`; // clustore url
+const connectionString = `mongodb+srv://mrunalugemuge:${password}@devcluster.iriyz7t.mongodb.net/?retryWrites=true&w=majority&appName=DevCluster`; // clustore url
 const client = new MongoClient(connectionString);
 let conn;
 try {
   conn = await client.connect();
-  console.log("connection successful")
+  console.log("Connection successful");
 } catch(e) {
-  console.error(e);
+  console.error("Error connecting to MongoDB:", e);
 }
 let db = conn.db("integration_ninjas");
 export default db;
